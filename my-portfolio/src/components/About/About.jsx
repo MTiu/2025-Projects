d8895972887135179dcb662b0d328f66fb1d56c0
Matthew-Tiu <mattjtiu@gmail.com>
2025-01-18 17:57:10 +0800

    About resolutions update commit

--- my-portfolio/src/components/About/About.jsx
+++ my-portfolio/src/components/About/About.jsx
@@ -5,37 +5,47 @@ import { carouselItems, animeCards, mangaCards, gameCards, techStack } from "./a
 
 function About() {
     return (
-        <Container>
+        <Container fluid>
             <Row className="mt-5 align-items-center flex-column gap-5">
                 <Col className="d-flex flex-column align-items-center gap-5">
-                    <h1 className="fs-1 fw-bold text-center">
+                    <h1 className="fs-1 text-info fw-bold text-center ">
                         My love for anime, games, and programming fuels my
                         creativity, problem-solving in development, and approach
                         to life's challenges.
                     </h1>
-                    <Carousel className={`${styles["custom-carousel"]}`}>
-                        {carouselItems.map((item, idx) => (
-                            <Carousel.Item key={idx}>
-                                <img
-                                    src={item.image}
-                                    alt={"Image of " + item.name}
-                                />
-                                <Carousel.Caption>
-                                    <h1>Image of {item.name}</h1>
-                                    <a
-                                        className="text-decoration-underline"
-                                        href={item.href}
-                                    >
-                                        {item.name} Image Link
-                                    </a>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        ))}
-                    </Carousel>
+                    <Container>
+                        <Carousel className={`${styles["custom-carousel"]}`}>
+                            {carouselItems.map((item, idx) => (
+                                <Carousel.Item key={idx}>
+                                    <img
+                                        src={item.image}
+                                        alt={"Image of " + item.name}
+                                    />
+                                    <Carousel.Caption>
+                                        <h1>Image of {item.name}</h1>
+                                        <a
+                                            className="text-decoration-underline"
+                                            href={item.href}
+                                        >
+                                            {item.name} Image Link
+                                        </a>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>
+                    </Container>
                 </Col>
-                <CustomCardGroup cardArray={animeCards} topic={"Animes"}></CustomCardGroup>
-                <CustomCardGroup cardArray={mangaCards} topic={"Mangas"}></CustomCardGroup>
-                <CustomCardGroup cardArray={gameCards} topic={"Games"}></CustomCardGroup>
+                <Row className="d-flex justify-content-center">
+                    <Col md="6" lg="4">
+                        <CustomCardGroup cardArray={animeCards} topic={"Animes"}></CustomCardGroup>
+                    </Col>
+                    <Col md="6" lg="4">
+                        <CustomCardGroup cardArray={mangaCards} topic={"Mangas"}></CustomCardGroup>
+                    </Col>
+                    <Col md="6" lg="4">
+                        <CustomCardGroup cardArray={gameCards} topic={"Games"}></CustomCardGroup>
+                    </Col>
+                </Row>
             </Row>
         </Container>
     );
--- my-portfolio/src/components/ui/CustomCardGroup/CustomCardGroup.jsx
+++ my-portfolio/src/components/ui/CustomCardGroup/CustomCardGroup.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Divider from '../../../assets/images/divider.png';
 import { Col, Card } from "react-bootstrap"
 import styles from "./customCardGroup.module.css"
 
 export default function CustomCardGroup( { cardArray, topic } ) {
 return (
 <div>
-    <Col className="d-flex flex-column align-items-center gap-5">
-        <img src={Divider} className={`${styles["custom-divider"]}`} alt="Image of page divider" />
-        <h1 className="fs-1 fw-bold">{topic} that I like</h1>
+    <Col className="mb-5 d-flex flex-column align-items-center gap-5">
+        <h1 className="mt-5 fs-2 fw-bold">{topic} that I like</h1>
         {
             cardArray.map((item,idx) => (
                 <Card className={`${styles["custom-card"]} d-flex flex-row border-info`} key={idx} data-bs-theme="dark">
