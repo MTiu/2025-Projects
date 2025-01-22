0429ecf84a15a2c332cfa3107975c2be501b905b
Matthew-Tiu <mattjtiu@gmail.com>
2025-01-22 20:34:14 +0800

    Tech Stack Update Commit

--- my-portfolio/src/components/About/About.jsx
+++ my-portfolio/src/components/About/About.jsx
@@ -2,6 +2,7 @@ import { Carousel, Container, Col, Row } from "react-bootstrap";
 import CustomCardGroup from "../ui/CustomCardGroup/CustomCardGroup";
 import styles from "./about.module.css"
 import { carouselItems, animeCards, mangaCards, gameCards, techStack } from "./aboutData";
+import FadeIn from "../ui/FadeInGroup/FadeIn";
 
 function About() {
     return (
@@ -9,30 +10,45 @@ function About() {
             <Row className="mt-5 align-items-center flex-column gap-5">
                 <Col className="d-flex flex-column align-items-center gap-5">
                     <h1 className="fs-1 text-info fw-bold text-center ">
+                        Throughout my career I experienced challenges and tackled them
+                        and those made me the programmer that I am today and expanded my tech stack
+                    </h1>
+                    <Container fluid className="border border-info rounded">
+                        <Row className="m-5 align-items-center gap-4 p-5 justify-content-center">
+                            {techStack.map((item,idx)=>(
+                                <Col className="mb-4" sm="4" md="3" lg="2" xxl="1" key={idx}><img src={item.image} alt={"Image of " + item.name} /></Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </Col>
+                <Col style={{marginTop:"7%"}} className="d-flex flex-column align-items-center gap-5">
+                    <h1 className="fs-1 text-info fw-bold text-center ">
                         My love for anime, games, and programming fuels my
                         creativity, problem-solving in development, and approach
                         to life's challenges.
                     </h1>
                     <Container>
-                        <Carousel className={`${styles["custom-carousel"]}`}>
-                            {carouselItems.map((item, idx) => (
-                                <Carousel.Item key={idx}>
-                                    <img
-                                        src={item.image}
-                                        alt={"Image of " + item.name}
-                                    />
-                                    <Carousel.Caption>
-                                        <h1>Image of {item.name}</h1>
-                                        <a
-                                            className="text-decoration-underline"
-                                            href={item.href}
-                                        >
-                                            {item.name} Image Link
-                                        </a>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                            ))}
-                        </Carousel>
+                        <FadeIn>
+                            <Carousel className={`${styles["custom-carousel"]}`}>
+                                {carouselItems.map((item, idx) => (
+                                    <Carousel.Item key={idx}>
+                                        <img
+                                            src={item.image}
+                                            alt={"Image of " + item.name}
+                                        />
+                                        <Carousel.Caption>
+                                            <h1>Image of {item.name}</h1>
+                                            <a
+                                                className="text-decoration-underline"
+                                                href={item.href}
+                                            >
+                                                {item.name} Image Link
+                                            </a>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+                        </FadeIn>
                     </Container>
                 </Col>
                 <Row className="d-flex justify-content-center">
--- my-portfolio/src/components/About/aboutData.js
+++ my-portfolio/src/components/About/aboutData.js
@@ -2,6 +2,22 @@ import Fallout from "../../assets/images/fallout.jpg";
 import Lycoris from "../../assets/images/lycoris.webp";
 import Wuwa from "../../assets/images/wuwa.jpg";
 
+import Bootstrap from "../../assets/images/tech-stack/bootstrap.svg"
+import CodeIgniter from "../../assets/images/tech-stack/codeigniter.svg"
+import CSS from "../../assets/images/tech-stack/css3.svg"
+import GIT from "../../assets/images/tech-stack/git.svg"
+import HTML5 from "../../assets/images/tech-stack/html5.svg"
+import JavaScript from "../../assets/images/tech-stack/javascript.svg"
+import MongoDB from "../../assets/images/tech-stack/mongodb.svg"
+import MySQL from "../../assets/images/tech-stack/mysql.svg"
+import NodeJS from "../../assets/images/tech-stack/nodedotjs.svg"
+import PHP from "../../assets/images/tech-stack/php.svg"
+import React from "../../assets/images/tech-stack/react.svg"
+import Redux from "../../assets/images/tech-stack/redux.svg"
+import Ruby from "../../assets/images/tech-stack/ruby.svg"
+import RoR from "../../assets/images/tech-stack/rubyonrails.svg"
+import WordPress from "../../assets/images/tech-stack/wordpress.svg"
+
 export const carouselItems = [
     {
         name: "Wuthering Waves",
@@ -23,17 +39,14 @@ export const carouselItems = [
 export const animeCards = [
     {
         name: "Lycoris Recoil",
-        href: "https://picfiles.alphacoders.com/543/543540.jpg",
         image: "https://picfiles.alphacoders.com/543/543540.jpg",
     },
     {
         name: "Frieren: Beyond Journey's End",
-        href: "https://th.bing.com/th/id/OIP.NQGdChhstqQrlJlV9eGgOAAAAA?w=426&h=600&rs=1&pid=ImgDetMain",
-        image: "https://th.bing.com/th/id/OIP.NQGdChhstqQrlJlV9eGgOAAAAA?w=426&h=600&rs=1&pid=ImgDetMain",
+        image: "https://th.bing.com/th/id/OIP.oh9RfharLX_VGHx651p_QgHaLH?rs=1&pid=ImgDetMain",
     },
     {
         name: "Bofuri",
-        href: "https://www.animeclick.it/immagini/anime/Itai_no_wa_Iya_nano_de_Bougyoryoku_ni_Kyokufuri_Shitai_to_Omoimasu/cover/Itai_no_wa_Iya_nano_de_Bougyoryoku_ni_Kyokufuri_Shitai_to_Omoimasu-cover.jpg",
         image: "https://www.animeclick.it/immagini/anime/Itai_no_wa_Iya_nano_de_Bougyoryoku_ni_Kyokufuri_Shitai_to_Omoimasu/cover/Itai_no_wa_Iya_nano_de_Bougyoryoku_ni_Kyokufuri_Shitai_to_Omoimasu-cover.jpg",
     },
 ];
@@ -42,17 +55,14 @@ export const animeCards = [
 export const mangaCards = [
     {
         name: "I Favor the Villainess",
-        href: "https://th.bing.com/th/id/OIP.BPJrTTVo_EhgC6aflU0Z7wHaKY?rs=1&pid=ImgDetMain",
         image: "https://th.bing.com/th/id/OIP.BPJrTTVo_EhgC6aflU0Z7wHaKY?rs=1&pid=ImgDetMain",
     },
     {
         name: "Watanare",
-        href: "https://1.bp.blogspot.com/-fuCa9DxplWk/YS8uC0rro3I/AAAAAAAAAQk/mmspfxQ28_Y9rAWd9bJEs0yGKoj2KDa1QCNcBGAsYHQ/s1800/cover.jpg",
         image: "https://1.bp.blogspot.com/-fuCa9DxplWk/YS8uC0rro3I/AAAAAAAAAQk/mmspfxQ28_Y9rAWd9bJEs0yGKoj2KDa1QCNcBGAsYHQ/s1800/cover.jpg",
     },
     {
         name: "Whisper Me a Love Song",
-        href: "https://temp.compsci88.com/cover/Whisper-Me-a-Love-Song.jpg",
         image: "https://temp.compsci88.com/cover/Whisper-Me-a-Love-Song.jpg",
     },
 ];
@@ -61,17 +71,14 @@ export const mangaCards = [
 export const gameCards = [
     {
         name: "Wuthering Waves",
-        href: "https://gamebrott.com/wp-content/uploads/2024/05/Build-Rover-Spectro-Wuthering-Waves-Terbaik-1-1024x576.webp",
         image: "https://gamebrott.com/wp-content/uploads/2024/05/Build-Rover-Spectro-Wuthering-Waves-Terbaik-1-1024x576.webp",
     },
     {
         name: "Fallout 4",
-        href: "https://th.bing.com/th/id/OIP.i7lP3qwJ8-4GwwGQoDeF7wHaHa?rs=1&pid=ImgDetMain",
         image: "https://th.bing.com/th/id/OIP.i7lP3qwJ8-4GwwGQoDeF7wHaHa?rs=1&pid=ImgDetMain",
     },
     {
         name: "World Flipper",
-        href: "https://peloqstore.com/wp-content/uploads/2023/04/World-Flipper-1080x1080.png",
         image: "https://peloqstore.com/wp-content/uploads/2023/04/World-Flipper-1080x1080.png",
     },
 ];
@@ -80,39 +87,78 @@ export const gameCards = [
 export const techStack = [
     {
         name: "HTML",
-        image: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911339/html5-icon-icon-md.png",
+        image: HTML5,
     },
+
     {
         name: "CSS",
-        image: "https://icon-library.com/images/css3-icon/css3-icon-28.jpg",
+        image: CSS,
     },
+
     {
         name: "JavaScript",
-        image: "https://static.vecteezy.com/system/resources/previews/027/127/560/original/javascript-logo-javascript-icon-transparent-free-png.png",
+        image: JavaScript,
     },
+
     {
         name: "React",
-        image: "https://raw.githubusercontent.com/Buuntu/fastapi-react/master/assets/react-logo.png",
+        image: React,
     },
+
+    {
+        name: "Redux",
+        image: Redux,
+    },
+
+    {
+        name: "MongoDB",
+        image: MongoDB,
+    },
+
+    { 
+        name: "PHP", 
+        image: PHP,
+    },
+
     {
         name: "MySQL",
-        image: "https://pngimg.com/uploads/mysql/mysql_PNG23.png",
+        image: MySQL,
     },
-    { name: "PHP", image: "https://pngimg.com/uploads/php/php_PNG43.png" },
+
+    {
+        name: "CodeIgniter",
+        image: CodeIgniter,
+    },
+
+    {
+        name: "WordPress",
+        image: WordPress,
+    },
+
     {
         name: "Ruby",
-        image: "https://images.vexels.com/media/users/3/166485/isolated/preview/d4061b653e6ba02ad0afdc79e0315a25-ruby-programming-language-icon-by-vexels.png",
+        image: Ruby,
     },
+
     {
         name: "Ruby on Rails",
-        image: "https://th.bing.com/th/id/OIP.AEZuEdPLzc_6LD78AafPBAHaHJ?rs=1&pid=ImgDetMain",
+        image: RoR,
     },
+
     {
         name: "Bootstrap",
-        image: "https://i.pinimg.com/originals/f1/9a/48/f19a489c91c70ce40cce8fcfbeb7143b.png",
+        image: Bootstrap,
     },
+
     {
         name: "NodeJS",
-        image: "https://eladnava.com/content/images/2016/04/nodejs-6.jpg",
+        image: NodeJS,
     },
+
+    {
+        name: "Git",
+        image: GIT,
+    },
+
+    
 ];
